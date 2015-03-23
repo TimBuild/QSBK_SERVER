fd19ee3b6b1d4072c4923b3f0a39fe65b0a695f6
brad <symworkd@163.com>
2015-03-23 15:57:04 +0800

    modify UserService.java

--- QiuBai/src/com/qiubai/service/UserService.java
+++ QiuBai/src/com/qiubai/service/UserService.java
@@ -1,5 +1,31 @@
 package com.qiubai.service;
 
+import javax.ws.rs.FormParam;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/UserService")
 public class UserService {
 
+	/**
+	 * user register
+	 * 
+	 * @param username
+	 * @param password
+	 * @return
+	 */
+	@POST
+	@Path("/register")
+	@Produces(value = MediaType.TEXT_PLAIN)
+	public String register(@FormParam("email") String email,
+			@FormParam("nickname") String nickname,
+			@FormParam("password") String password) {
+		System.out.println(email);
+		System.out.println(nickname);
+		System.out.println(password);
+
+		return "success";
+	}
 }
